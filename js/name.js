7695b08ae517606e2dc1777977e3e19c624f76ca
Derulo <96000014+DeruL0@users.noreply.github.com>
2023-10-09 21:59:44 +0800

    Update name.js

--- js/name.js
+++ js/name.js
@@ -8,7 +8,7 @@ async function fetchCSVData(url) {
 }
 
 // 处理 CSV 数据并创建图表
-fetchCSVData('/datasets/name.csv').then(function (csvData) {
+fetchCSVData('https://github.com/DeruL0/test1.github.io/blob/master/datasets/name.csv').then(function (csvData) {
     // 将 CSV 数据解析为数组
     var lines = csvData.split('\n');
     var labelCounts = {};
